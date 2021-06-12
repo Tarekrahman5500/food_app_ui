aa6af3a95d12411b18649ec86c13d0a4b107eecc
tarek <tr617739@gmail.com>
2021-06-13 00:11:45 +0600

    Basic Details

---
+++ app/src/main/java/com/example/foodorderui/Activity/ShowDetailActivity.java
@@ -0,0 +1,18 @@
+package com.example.foodorderui.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.foodorderui.databinding.ActivityShowDetailBinding;
+
+import java.util.Objects;
+
+public class ShowDetailActivity extends AppCompatActivity {
+   ActivityShowDetailBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityShowDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Objects.requireNonNull(getSupportActionBar()).hide();
+    }
+}
--- app/src/main/java/com/example/foodorderui/MainActivity.java
+++ app/src/main/java/com/example/foodorderui/MainActivity.java
@@ -2,11 +2,20 @@ package com.example.foodorderui;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.foodorderui.Adapter.CategoryAdapter;
+import com.example.foodorderui.Adapter.PopularAdapter;
+import com.example.foodorderui.Domain.CategoryDomain;
+import com.example.foodorderui.Domain.FoodDomain;
 import com.example.foodorderui.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView.Adapter adapter, adapter2;
 
     ActivityMainBinding binding;
     @Override
@@ -16,5 +25,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         Objects.requireNonNull(getSupportActionBar()).hide();
 
+        recyclerViewCategoryList();
+        recyclerViewPopularList();
+
+    }
+
+    private void recyclerViewPopularList() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,
+                false);
+
+        binding.recyclerView2.setLayoutManager(linearLayoutManager);
+        ArrayList<FoodDomain> foodlist = new ArrayList<>();
+        foodlist.add(new FoodDomain("Pepperoni pizza", "pizza", "slices pepperoni ,mozzarella cheese, fresh oregano,  ground black pepper, pizza sauce", 9.76));
+        foodlist.add(new FoodDomain("Cheese Burger", "burger", "beef, Gouda Cheese, Special sauce, Lettuce, tomato ", 8.79));
+        foodlist.add(new FoodDomain("Vegetable pizza", "pizza2", " olive oil, Vegetable oil, pitted Kalamata, cherry tomatoes, fresh oregano, basil", 8.5));
+
+        adapter2 = new PopularAdapter(foodlist);
+        binding.recyclerView2.setAdapter(adapter2);
+    }
+
+    private void recyclerViewCategoryList() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,
+                false);
+
+        binding.recyclerView.setLayoutManager(linearLayoutManager);
+
+        ArrayList<CategoryDomain> categoryDomains = new ArrayList<>();
+        categoryDomains.add(new CategoryDomain("pizza", "cat_1"));
+        categoryDomains.add(new CategoryDomain("Burger", "cat_2"));
+        categoryDomains.add(new CategoryDomain("Hotdog", "cat_3"));
+        categoryDomains.add(new CategoryDomain("Drink", "cat_4"));
+        categoryDomains.add(new CategoryDomain("Donat", "cat_5"));
+
+        adapter = new CategoryAdapter(categoryDomains);
+        binding.recyclerView.setAdapter(adapter);
     }
 }
